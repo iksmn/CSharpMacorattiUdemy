9eeafcc2a24a213c3885b671a756902b29f177de
Icaro Kossman <icaro.kossman@gcs.local>
2026-04-29 13:49:12 -0300

    primeiro commit

---
+++ CSharpMacorattiUdemy/Exemplos.cs
@@ -0,0 +1,26 @@
+/*
+Console.WriteLine("## Classes e Métodos ##\n");
+
+Pessoa pessoa1 = new();
+pessoa1.Nome = "Henrique";
+pessoa1.Idade = 38;
+pessoa1.Nacionalidade = "Brasileira";
+
+Pessoa pessoa2 = new();
+pessoa2.Nome = "Karl";
+pessoa2.Idade = 26;
+pessoa2.Nacionalidade = "Alemã";
+
+Console.WriteLine($"{pessoa1.Nome}, {pessoa1.Idade}, {pessoa1.Nacionalidade}");
+Console.WriteLine($"{pessoa2.Nome}, {pessoa2.Idade}, {pessoa2.Nacionalidade}");
+
+
+Console.ReadKey();
+class Pessoa
+{
+    public string? Nome;
+    public int Idade;
+    public string? Nacionalidade;
+}
+*/
+
